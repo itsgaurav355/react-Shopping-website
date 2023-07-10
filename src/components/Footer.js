d254c41463b4f2b659b37a1093357dba9a27c648
itsgaurav355 <prajapatigaurav355@gmail.com>
2023-07-10 21:51:38 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {BsLinkedin,BsGithub,BsYoutube,BsInstagram} from 'react-icons/bs'
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-item-center">
+                <img src="images/newsletter.png" alt="Newsletter" />
+                <h2 className="mb-0 text-white">Sign Up for Newsletter </h2>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control py-1"
+                  placeholder="Enter your Email"
+                  aria-label="Enter your Email"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div>
+                <address className="text-white fs-6">
+                  PanchSheel Seva samiti <br /> Indira Nagar, Lalji <br /> pada
+                  Kandivali west <br />
+                  Mumbai-67
+                </address>
+                <a
+                  href="tel:+91 8657238954"
+                  className="mt-3 d-block mb-1 text-white"
+                >
+                  +91 8657238954
+                </a>
+                <a
+                  href="mailto:prajapatigaurav355@gmail.com"
+                  className="mt-2 d-block mb-0 text-white"
+                >
+                  prajapatigaurav355@gmail.com
+                </a>
+                <div className="social_icons d-flex align-items-center gap-30 mt-4">
+                  <a className="text-white" href="#"><BsLinkedin className="fs-4"/></a>
+                  <a className="text-white" href="#"><BsYoutube className="fs-4"/></a>
+                  <a className="text-white" href="#"><BsInstagram className="fs-4"/></a>
+                  <a className="text-white" href="#"><BsGithub className="fs-4"/></a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Information</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Privacy Policy</Link>
+                <Link className="text-white py-2 mb-1">Refund Policy</Link>
+                <Link className="text-white py-2 mb-1">Shipping Policy</Link>
+                <Link className="text-white py-2 mb-1">Terms & Conditions</Link>
+                <Link className="text-white py-2 mb-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h4 className="text-white mb-4">Account</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">About Us</Link>
+                <Link className="text-white py-2 mb-1">FAQ</Link>
+                <Link className="text-white py-2 mb-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-2 mb-1">Laptops</Link>
+                <Link className="text-white py-2 mb-1">Headphones</Link>
+                <Link className="text-white py-2 mb-1">Tablets</Link>
+                <Link className="text-white py-2 mb-1">Watch</Link>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; {new Date().getFullYear()} Powered by Apna Bazaar
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/ProductCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+
+const ProductCard = () => {
+  return (
+    <div className="col-3">
+      <Link className="product-card position-relative">
+        <div className="wishlist-icon position-absolute">
+          <Link>
+            <img src="images/wish.svg" alt="Wishlist" />
+          </Link>
+        </div>
+        <div className="product-image">
+          <img src="images/watch.jpg"  className="img-fluid" alt="Product Image" />
+          <img src="images/watch-1.jpg" className="img-fluid"  alt="Product Image" />
+        </div>
+        <div className="product-details">
+          <h6 className="brand">Havels</h6>
+          <h5 className="product-title">
+            Kids headphones bulk 10 packmulti coloured for students
+          </h5>
+          <ReactStars
+            count={5}
+            size={24}
+            value={3}
+            edit={false}
+            activeColor="#ffd700"
+          />
+          <p className="price">$100.00</p>
+        </div>
+        <div className="action-bar position-absolute">
+          <div className="d-flex flex-column gap-15">
+            <Link>
+              <img src="images/add-cart.svg" alt="add-cart" />
+            </Link>
+            <Link>
+              <img src="images/view.svg" alt="view" />
+            </Link>
+            <Link>
+              <img src="images/prodcompare.svg" alt="Compare" />
+            </Link>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductCard;
